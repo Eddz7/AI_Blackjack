147221bb3c6f7fab6f410f32c5851a7efa288b62
Edison <ediesonchen@gmail.com>
2024-03-02 22:43:38 -0800

    added better deck init, random shuffling, and get_value()

--- blackjack.py
+++ blackjack.py
@@ -9,32 +9,48 @@ screen_width, screen_height = 800, 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 icon = pygame.image.load('resources/icon.png')
-pygame.display.set_icon(icon)
 cardback = pygame.image.load('resources/cards/cardback.png')
+pygame.display.set_icon(icon)
 
 # Define Card class
 class Card:
     def __init__(self, name):
         self.name = name
+        self.value = self.get_value()
+
+    def get_value(self):
+        # this tries to convert the entire string except for the suit to an int
+        if self.name[:-1].isnumeric():
+            return int(self.name[:-1])
+        elif self.name[0] in ['j', 'q', 'k']:
+            return 10
+        # future implementation: use a boolean to check if hand has an ace and total value > 21, then subtract 10
+        elif self.name[0] == 'a':
+            return 11
+        else:
+            raise ValueError("Invalid card name")
 
     def load_image(self):
         return pygame.image.load(f'resources/cards/{self.name}.png')
 
     @staticmethod
     def generate_deck():
-        card_names = ['ad', 'ac', 'ah', 'as', '2d', '2c', '2h', '2s',
-                      '3d', '3c', '3h', '3s', '4d', '4c', '4h', '4s', '5d', '5c', '5h',
-                      '5s', '6d', '6c', '6h', '6s', '7d', '7c', '7h', '7s', '8d', '8c',
-                      '8h', '8s', '9d', '9c', '9h', '9s', '10d', '10c', '10h', '10s',
-                      'jd', 'jc', 'jh', 'js', 'qd', 'qc', 'qh', 'qs', 'kd', 'kc', 'kh', 'ks']
-        deck = [Card(name) for name in card_names]
+        numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a']
+        suits = ['c', 'd', 'h', 's']
+        deck = [Card(f'{number}{suit}') for number in numbers for suit in suits]
+        random.shuffle(deck)
         return deck
 
 # Load card images
 deck = Card.generate_deck()
+
 for card in deck:
     card.image = card.load_image()
 
+total = 0
+for i in range(len(deck)):
+    total += deck[i].get_value()
+print(total)
 # Display first card image
 first_card_image = deck[0].image
 screen.blit(first_card_image, (screen_width//2 - first_card_image.get_width()//2, screen_height//2 - first_card_image.get_height()//2))
