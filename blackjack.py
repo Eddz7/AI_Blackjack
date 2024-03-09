ed7e86573be42bf6cda422ed704d465f02c24ac8
Edison <ediesonchen@gmail.com>
2024-03-09 10:54:00 -0800

    pygame init

--- blackjack.py
+++ blackjack.py
@@ -33,7 +33,7 @@ class Blackjack:
         value = 0
         aces = 0
         for card in hand:
-            # this tries to convert the entire string except for the suit to an int
+            #this tries to convert the entire string except for the suit to an int
             if card.name[:-1].isnumeric():
                 value += int(card.name[:-1])
             elif card.name[0] in ['j', 'q', 'k']:
@@ -41,14 +41,49 @@ class Blackjack:
             elif card.name[0] == 'a':
                 value += 11
                 aces += 1
-        # blackjack aces implementation
+        #blackjack aces implementation
         while value > 21 and aces:
             value -= 10
             aces -= 1
         return value
 
 def main():
-    pass
+    #initialize pygame
+    pygame.init()
+    #set display dimensions
+    screen_width, screen_height = 900, 700
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    #load icon and cardback images
+    icon = pygame.image.load('resources/icon.png')
+    cardback = pygame.image.load('resources/cards/cardback.png')
+    pygame.display.set_caption('Blackjack')
+    pygame.display.set_icon(icon)
+    #initialize background
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    gray = (128,128,128)
+    background_color = (134, 36, 42)
+    background = pygame.Surface(screen.get_size())
+    background.fill(background_color)
+    #initialize buttons and text
+    font = pygame.font.SysFont('arial', 15)
+    hit_txt = font.render('Hit', 1, black)
+    stand_txt = font.render('Stand', 1, black)
+    restart_txt = font.render('Restart', 1, white)
+    gameover_txt = font.render('GAME OVER', 1, (255,255,255))
+    #display background and hit/stand buttons
+    hit_b = pygame.draw.rect(background, (gray), (360, 450, 80, 30))
+    stand_b = pygame.draw.rect(background, (gray), (460, 450, 80, 30))
+    screen.blit(background, (0, 0))
+    screen.blit(hit_txt, (392, 455))
+    screen.blit(stand_txt, (484, 455))
+    pygame.display.update()
+
+    #main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
 
 if __name__ == '__main__':
     main()
